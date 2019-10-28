ea3bdd64628c8d7f74cb089777b69a0dfcd471c7
Florian ORTBAUER <Florian.Ortbauer@knapp.com>
2019-10-28 15:29:55 +0100

    Changed btns

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { SiteModelComponent } from './components/site-model/site-model.component';
 
 
+
 const routes: Routes = [
+  
   { path: 'site-model', component: SiteModelComponent },
+
 ];
 
 @NgModule({
