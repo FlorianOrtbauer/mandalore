67c857f0dbb0e1c8d72e7371e5b596b2c622552a
Florian Ortbauer <florian.ortbauer@edu.campus02.at>
2019-11-09 08:39:58 +0100

    Added routing for organisation configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { SiteModelComponent } from './components/site-model/site-model.component';
+import { ClientConfigurationComponent } from './components/client-configuration/client-configuration.component';
 import { LoginComponent } from './components/login/login.component';
 
 
@@ -11,8 +12,8 @@ const routes: Routes = [
   // main app routes
   { path: '', component: LoginComponent},
   { path: 'cmms', component: SidenavComponent},
-  { path: 'site-model', component: SiteModelComponent, outlet:"content" }
-  
+  { path: 'site-model', component: SiteModelComponent, outlet:"content" },
+  { path: 'client-configuration', component: ClientConfigurationComponent, outlet:"content" }
 ];
 
 @NgModule({
