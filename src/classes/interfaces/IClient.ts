8bd24779fd353691fb7b9c1070090f71e465c890
Simon Bauer-Kieslinger <s.bauer-kieslinger@edu.campus02.at>
2019-12-27 15:23:17 +0100

    Interface Clients

---
+++ src/classes/interfaces/IClient.ts
@@ -0,0 +1,10 @@
+export interface IClient {
+  id: string;
+  name: string;
+  priority: string;
+  country: string;
+  address_line_1: string;
+  address_line_2: string;
+  address_line_3: string;
+  address_line_4: string;
+}
